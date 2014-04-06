5b233a2332a56dd0c56aa7eefaca52b172266fd0
Cheima <Cheima@pc-cheima>
2014-04-07 01:36:34 +0200

    1ere version

--- PfeFrontEnd/app/js/controllers.js
+++ PfeFrontEnd/app/js/controllers.js
@@ -5,7 +5,10 @@
 angular.module('myApp.controllers', []).
   controller('MyCtrl1', function($scope, trafficforsigu) {
         $scope.alltraffics = trafficforsigu.query();
+        $scope.showSelectedElement = function(s){
+           $scope.selected = s; 
+        };
   })
-  .controller('MyCtrl2', [function() {
-
-  }]);
+  .controller('MyCtrl2', function($scope, trafficforsigu) {
+          $scope.alltraffics = trafficforsigu.query();
+  });
