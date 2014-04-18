a9ccaf37456ada1cbcc1f30153bf72f73f956e3d
Cheima <Cheima@pc-cheima>
2014-04-18 10:27:48 +0200

    displayOneSigu

--- PfeFrontEnd/app/js/services.js
+++ PfeFrontEnd/app/js/services.js
@@ -2,7 +2,7 @@
 
 var trafficServices =angular.module('myApp.services', ['ngResource']);
 trafficServices.factory('trafficforsigu',function($resource){
-    return($resource('http://localhost:9999/upa-test-web/webresources/generic/traffic',{},{
+    return($resource('http://localhost:9999/upa-test-web/webresources/generic/siguname',{},{
       query:{method:'GET',isArray:true}  
     }));
 });
