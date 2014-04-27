5ed85550f6b6f1fc2250bea98840797b60fce5e8
Cheima <Cheima@pc-cheima>
2014-04-27 12:52:58 +0200

    affichertouslessigus

--- mss-dashboard/app/js/controllers.js
+++ mss-dashboard/app/js/controllers.js
@@ -9,120 +9,126 @@ google.setOnLoadCallback(function() {
 
 angular.module('myApp.controllers', []).
         controller('MyCtrl1', function($scope, trafficforsigu, getsigunames, getalltraffic, allgraphs) {
-
-            // var nom = "SIGU 0";
-            //  $scope.alltraffics = trafficforsigu.query({siguId : "SIGU-3"});
-            // $scope.siguSelected = 56;
             $scope.graphs = allgraphs.query();
-            
-             $scope.graphs.$promise.then(function(result) {
-                    $scope.graphs = result;
-                    //  var i = 0;
-                    var val = [['Time', 'TrafficIN']];
-                    // key = i && tab[i]=result[key]
-                    for (var key in result) {
-                        var obj = result[key];
-                     //   if( obj !== result[key-1]){
-                                       
-                        //  i = i + 5;
-                        var d = new Date(obj["dateExec"]);
+
+            $scope.graphs.$promise.then(function(result) {
+                $scope.graphs = result;
+                //  var i = 0;
+                
+                // key = i && tab[i]=result[key]
+                for (var key in result) {
+                   // alert(key);
+                    var obj = result[key];
+                    var liste = obj["liste"];
+                    
+                    for (var key1 in liste)
+                    {
+                        alert(key1);
+                        var val = [['Time', obj["siguname"]]];
+                        var obj1 = liste[key1];
+                        var d = new Date(obj1["dateExec"]);
                         var d1 = d.getHours();
                         var d2 = d.getMinutes();
                         var curr_date = d.getDate();
                         var curr_month = d.getMonth() + 1; //Months are zero based
                         var curr_year = d.getFullYear();
                         var datenow = curr_date + "-" + curr_month + "-" + curr_year;
-
-                        val.push([d1 + ":" + d2, obj["packetreceived"]]);
+                        
+                        val.push([d1 + ":" + d2, obj1["packetreceived"]]);
+                       // alert(val);
                         // val.push([i, obj["packetreceived"]]);
                         //}
                         // }
                         // else {
                         //    i++;
                         // }
-                    
-                   // }
-                    var data = google.visualization.arrayToDataTable(val);
-                    var options = {
-                        title: 'TrafficIN for SIGU'
-                    };
-                    var chart = {};
-                    chart.data = data;
-                    chart.options = options;
 
-                    $scope.chartTypes = [
-                        {typeName: 'LineChart', typeValue: '1'},
-                        {typeName: 'BarChart', typeValue: '2'},
-                        {typeName: 'ColumnChart', typeValue: '3'},
-                        {typeName: 'PieChart', typeValue: '4'}
-                    ];
-                    $scope.selectType = function(type) {
-                        $scope.chart.type = type.typeValue;
-                    };
-                    chart.type = $scope.chartTypes[0].typeValue;
-                    $scope.chartType = $scope.chartTypes[0];
+                        // }
+                        var data = google.visualization.arrayToDataTable(val);
+                        
 
-                    $scope.chart = chart;
-                
-                }});
+                   
+               
+                var options = {
+                    title: 'TrafficIN for SIGU'
+                };
+                var chart = {};
+                chart.data = data;
+                chart.options = options;
+
+                $scope.chartTypes = [
+                    {typeName: 'LineChart', typeValue: '1'},
+                    {typeName: 'BarChart', typeValue: '2'},
+                    {typeName: 'ColumnChart', typeValue: '3'},
+                    {typeName: 'PieChart', typeValue: '4'}
+                ];
+                $scope.selectType = function(type) {
+                    $scope.chart.type = type.typeValue;
+                };
+                chart.type = $scope.chartTypes[0].typeValue;
+                $scope.chartType = $scope.chartTypes[0];
+
+                $scope.chart = chart;
+
+                    }  }  });
 
             $scope.sigunames = getsigunames.query();
             $scope.traffic = getalltraffic.query();
-          /*  $scope.update = function() {
-                //alert($scope.siguSelected);
-                $scope.alltraffics = trafficforsigu.query({siguId: $scope.siguSelected});
-                $scope.alltraffics.$promise.then(function(result) {
-                    $scope.alltraffics = result;
-                    //  var i = 0;
-                    var val = [['Time', 'TrafficIN']];
-                    // key = i && tab[i]=result[key]
-                    for (var key in result) {
-                        var obj = result[key];
-                        //  if (obj["siguName"] == nom) {                   
-                        //  i = i + 5;
-                        var d = new Date(obj["dateExec"]);
-                        var d1 = d.getHours();
-                        var d2 = d.getMinutes();
-                        var curr_date = d.getDate();
-                        var curr_month = d.getMonth() + 1; //Months are zero based
-                        var curr_year = d.getFullYear();
-                        var datenow = curr_date + "-" + curr_month + "-" + curr_year;
+              $scope.update = function() {
+             //alert($scope.siguSelected);
+             $scope.alltraffics = trafficforsigu.query({siguId: $scope.siguSelected});
+             $scope.alltraffics.$promise.then(function(result) {
+             $scope.alltraffics = result;
+             //  var i = 0;
+             var val = [['Time', 'TrafficIN']];
+             // key = i && tab[i]=result[key]
+             for (var key in result) {
+             var obj = result[key];
+             //  if (obj["siguName"] == nom) {                   
+             //  i = i + 5;
+             var d = new Date(obj["dateExec"]);
+             var d1 = d.getHours();
+             var d2 = d.getMinutes();
+             var curr_date = d.getDate();
+             var curr_month = d.getMonth() + 1; //Months are zero based
+             var curr_year = d.getFullYear();
+             var datenow = curr_date + "-" + curr_month + "-" + curr_year;
+             
+             val.push([d1 + ":" + d2, obj["packetreceived"]]);
+             // val.push([i, obj["packetreceived"]]);
+             //}
+             // }
+             // else {
+             //    i++;
+             // }
+             }
+             var data = google.visualization.arrayToDataTable(val);
+             var options = {
+             title: 'TrafficIN for SIGU'
+             };
+             var chart = {};
+             chart.data = data;
+             chart.options = options;
+             
+             $scope.chartTypes = [
+             {typeName: 'LineChart', typeValue: '1'},
+             {typeName: 'BarChart', typeValue: '2'},
+             {typeName: 'ColumnChart', typeValue: '3'},
+             {typeName: 'PieChart', typeValue: '4'}
+             ];
+             $scope.selectType = function(type) {
+             $scope.chart.type = type.typeValue;
+             };
+             chart.type = $scope.chartTypes[0].typeValue;
+             $scope.chartType = $scope.chartTypes[0];
+             
+             $scope.chart = chart;
+             });
+             
+             };
 
-                        val.push([d1 + ":" + d2, obj["packetreceived"]]);
-                        // val.push([i, obj["packetreceived"]]);
-                        //}
-                        // }
-                        // else {
-                        //    i++;
-                        // }
-                    }
-                    var data = google.visualization.arrayToDataTable(val);
-                    var options = {
-                        title: 'TrafficIN for SIGU'
-                    };
-                    var chart = {};
-                    chart.data = data;
-                    chart.options = options;
-
-                    $scope.chartTypes = [
-                        {typeName: 'LineChart', typeValue: '1'},
-                        {typeName: 'BarChart', typeValue: '2'},
-                        {typeName: 'ColumnChart', typeValue: '3'},
-                        {typeName: 'PieChart', typeValue: '4'}
-                    ];
-                    $scope.selectType = function(type) {
-                        $scope.chart.type = type.typeValue;
-                    };
-                    chart.type = $scope.chartTypes[0].typeValue;
-                    $scope.chartType = $scope.chartTypes[0];
-
-                    $scope.chart = chart;
-                });
-
-            };*/
 
 
-       
 
         })
         .controller('MyCtrl2', function($scope, trafficforsigu, getalltraffic) {
