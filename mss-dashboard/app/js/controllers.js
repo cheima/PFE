6a122188ba4d2244a1672f85f61933430248451c
Cheima <Cheima@pc-cheima>
2014-05-01 16:49:02 +0200

    displaydropbox

--- mss-dashboard/app/js/controllers.js
+++ mss-dashboard/app/js/controllers.js
@@ -9,14 +9,6 @@ google.setOnLoadCallback(function() {
 
 angular.module('myApp.controllers', []).
         controller('MyCtrl1', function($scope, trafficforsigu, getsigunames, toptraffic, allgraphs, siguranged) {
-            
-$scope.DropdownCtrl = function() {
-  $scope.items = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
-};
             $scope.sigunames = getsigunames.query();
             //                    
 
@@ -283,5 +275,52 @@ $scope.DropdownCtrl = function() {
 
 }).controller('MyCtrl4', function() {
 
+}).controller('Index2', function($scope) {
+    $scope.items = [
+        "The first choice!",
+        "And another choice for you.",
+        "but wait! A third!"
+    ];
+    
+    $scope.DatepickerDemoCtrl = function () {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.showWeeks = true;
+  $scope.toggleWeeks = function () {
+    $scope.showWeeks = ! $scope.showWeeks;
+  };
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = ( $scope.minDate ) ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    'year-format': "'yy'",
+    'starting-day': 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+  $scope.format = $scope.formats[0];
+};
 });
                
