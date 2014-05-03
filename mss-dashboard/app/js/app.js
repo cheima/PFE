5507b9b242adc322954c890fbbad9a62a07285d5
Cheima <Cheima@pc-cheima>
2014-05-03 19:52:38 +0200

    displaydropbox4

--- mss-dashboard/app/js/app.js
+++ mss-dashboard/app/js/app.js
@@ -4,17 +4,18 @@
 // Declare app level module which depends on filters, and services
 //angular.module('myApp', ['ui.bootstrap']);
 angular.module('myApp', [
-  'ui.bootstrap',
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+    'ui.bootstrap',
+    'ngRoute',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
+    'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/trafficIN', {templateUrl: 'partials/trafficin.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/trafficOUT', {templateUrl: 'partials/trafficout.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/LoadCPU', {templateUrl: 'partials/LoadCPU.html', controller: 'MyCtrl3'});
-  $routeProvider.when('/Bandwidth', {templateUrl: 'partials/partial4.html', controller: 'MyCtrl4'});
-  $routeProvider.otherwise({redirectTo: '/trafficIN'});
-}]);
+        config(['$routeProvider', function($routeProvider) {
+                $routeProvider.when('/sigu', {templateUrl: 'partials/sigu.html', controller: 'MyCtrl1'});
+                $routeProvider.when('/dashboard', {templateUrl: 'partials/trafficin.html', controller: 'MyCtrl1'});
+                $routeProvider.when('/bsu', {templateUrl: 'partials/trafficout.html', controller: 'MyCtrl2'});
+               // $routeProvider.when('/LoadCPU', {templateUrl: 'partials/LoadCPU.html', controller: 'MyCtrl3'});
+               // $routeProvider.when('/Bandwidth', {templateUrl: 'partials/partial4.html', controller: 'MyCtrl4'});
+                $routeProvider.otherwise({redirectTo: '/dashboard'});
+            }]);
