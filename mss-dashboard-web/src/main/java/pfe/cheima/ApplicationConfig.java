f69f2cdc66d2b29a41649247d1f46937b7e38824
Cheima <Cheima@pc-cheima>
2014-05-24 02:13:42 +0200

    tajribicnxtelnet

--- mss-dashboard-web/src/main/java/pfe/cheima/ApplicationConfig.java
+++ mss-dashboard-web/src/main/java/pfe/cheima/ApplicationConfig.java
@@ -37,6 +37,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(pfe.cheima.CPUWebService.class);
         resources.add(pfe.cheima.GenericResource.class);
         resources.add(pfe.cheima.MSSManagmentREST.class);
     }
